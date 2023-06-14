7fa153b24f440d031b6e7a38cb9dfddefcbc1b58
pxsswordprotected <jamesalford661@gmail.com>
2023-06-14 17:18:25 -0500

    task component styling

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Task from './components/Task';
 
 export default function App() {
   return (
@@ -11,6 +12,8 @@ export default function App() {
         <Text style= {styles.sectionTitle}>Today's Tasks</Text>
         <View style = {styles.items}>   
           {/* Where the tasks will go */}
+          <Task text={'Task 1'} />
+          <Task text={'Task 2'} />
         </View>
       </View>
     </View>
@@ -30,5 +33,7 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: 'bold',
   },
-  items: {},
+  items: {
+    marginTop: 30
+  },
 });
